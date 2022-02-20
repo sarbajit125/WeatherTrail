e5db4c8439c056a50ab15caaa71de35947854ea8
Talk2uttam14 <deyuttamkumar786@gmail.com>
2022-02-20 17:05:38 +0530

    Changed Hourly VC UI

--- WeatherTrail/View/ViewController.swift
+++ WeatherTrail/View/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var welcomebg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         let bg = UIImageView(frame: UIScreen.main.bounds)
         bg.image = UIImage(named: "first")
         bg.contentMode = UIView.ContentMode.scaleAspectFill
         self.view.insertSubview(bg, at: 0)
+        
+        
         // Do any additional setup after loading the view.
     }
 
